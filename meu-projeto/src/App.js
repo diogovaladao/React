4f88725a720235d56ab94ddd17dfee4f383955cb
Diogo Valadão <diogo.svaladao@gmail.com>
2022-08-22 09:47:08 -0300

    Fragments

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import SayMayName from './components/SayMyName';
-import Pessoa from './Pessoa';
+import Pessoa from './components/Pessoa';
 import Frase from './components/Fase';
+import List from './components/List';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <SayMayName nome="Doidão" />
       <SayMayName nome={nome} />
       <Pessoa nome="Diogo" idade="34" profissao="autônomo" foto="https://via.placeholder.com/150"/>
+      <List/>
     </div>
   );
 }
