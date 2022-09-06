b189e732bf741bde0f459c6421293444d1c21b62
Diogo Valadão <diogo.svaladao@gmail.com>
2022-09-06 15:00:58 -0300

    finalizado tela de cidade aula 24

--- cadastro/src/pages/cidades/ListagemDeCidade.tsx
+++ cadastro/src/pages/cidades/ListagemDeCidade.tsx
@@ -1,5 +1,27 @@
+import { useMemo } from "react";
+import { useSearchParams } from "react-router-dom";
+import { FerramentasDaListagem } from "../../shared/components";
+import { LayoutBaseDePagina } from "../../shared/layouts";
+
 export const ListagemDeCidade: React.FC= () => {
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const busca = useMemo(() => {
+        return searchParams.get('busca') || '';
+    }, [searchParams]);
+
     return(
-        <div>Teste listagem de cidades</div>
+        <LayoutBaseDePagina
+        titulo="Listagem de cidades"
+        barraDeFerramentas={<FerramentasDaListagem
+            mostraInputBusca
+            textoBotaoNovo="Nova"
+            textoDeBusca={busca}
+            aoMudarTextoDeBusca={texto => setSearchParams({busca: texto}, {replace: true})}
+        />}
+        >
+
+        </LayoutBaseDePagina>
     );
 };
