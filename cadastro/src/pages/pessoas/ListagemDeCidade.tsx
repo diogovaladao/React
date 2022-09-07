6163a5098086e58b2a7c6d6e7e0bae21b4b88af8
Diogo Valadão <diogo.svaladao@gmail.com>
2022-09-07 14:56:13 -0300

    Adicionando tabela aula 27

--- cadastro/src/pages/pessoas/ListagemDeCidade.tsx
+++ cadastro/src/pages/pessoas/ListagemDeCidade.tsx
@@ -1,28 +1,39 @@
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import { useSearchParams } from "react-router-dom";
+
+import { IListagemPessoa, PessoasServices } from "../../shared/services/api/pessoas/PessoasServices";
 import { FerramentasDaListagem } from "../../shared/components";
-import { useDebounce } from "../../shared/hooks";
 import { LayoutBaseDePagina } from "../../shared/layouts";
-import { PessoasServices } from "../../shared/services/api/pessoas/PessoasServices";
+import { useDebounce } from "../../shared/hooks";
 
 export const ListagemDePessoas: React.FC = () => {
 
     const [searchParams, setSearchParams] = useSearchParams();
     const { debounce } = useDebounce();
 
+    const [rows, setRows] = useState<IListagemPessoa[]>([]);
+    const [totalCount, setTotalCount] = useState(0);
+    const [isLoading, setIsLoading] = useState(true);
+
     const busca = useMemo(() => {
         return searchParams.get('busca') || '';
     }, [searchParams]);
 
     useEffect(() => {
+        setIsLoading(true);
 
         debounce(() => {
             /* usr o then trás o tipo do erro*/
             PessoasServices.getAll(1, busca).then((result) => {
+                setIsLoading(false);
                 if (result instanceof Error) {
                     alert(result.message);
                 } else {
                     console.log(result);
+
+                    setTotalCount(result.totalCount);
+                    setRows(result.data);
                 }
             })
         });
@@ -38,7 +49,28 @@ export const ListagemDePessoas: React.FC = () => {
                 aoMudarTextoDeBusca={texto => setSearchParams({ busca: texto }, { replace: true })}
             />}
         >
+            <TableContainer component={Paper} variant='outlined' sx={{m: 1, width: 'auto'}}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Ações</TableCell>
+                            <TableCell>Nome Completo</TableCell>
+                            <TableCell>Email</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
 
+                        {rows.map(row => (
+                            <TableRow key={row.id}>
+                                <TableCell>[ ]</TableCell>
+                                <TableCell>{row.nomeCompleto}</TableCell>
+                                <TableCell>{row.email}</TableCell>
+                            </TableRow>
+                        ))}
+
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </LayoutBaseDePagina>
     );
 };
