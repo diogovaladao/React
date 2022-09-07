c7370948d03c83e4fa71ab3a55a791a7dbb59075
Diogo Valadão <diogo.svaladao@gmail.com>
2022-09-07 11:00:10 -0300

    Como impedir consultas desnecessárias com o useDebounce aula 26

--- cadastro/src/pages/pessoas/ListagemDeCidade.tsx
+++ cadastro/src/pages/pessoas/ListagemDeCidade.tsx
@@ -1,37 +1,42 @@
 import { useEffect, useMemo } from "react";
 import { useSearchParams } from "react-router-dom";
 import { FerramentasDaListagem } from "../../shared/components";
+import { useDebounce } from "../../shared/hooks";
 import { LayoutBaseDePagina } from "../../shared/layouts";
 import { PessoasServices } from "../../shared/services/api/pessoas/PessoasServices";
 
-export const ListagemDePessoas: React.FC= () => {
+export const ListagemDePessoas: React.FC = () => {
 
     const [searchParams, setSearchParams] = useSearchParams();
+    const { debounce } = useDebounce();
 
     const busca = useMemo(() => {
         return searchParams.get('busca') || '';
     }, [searchParams]);
 
     useEffect(() => {
-        /* usr o then traz o tipo do erro*/
-        PessoasServices.getAll(1, busca).then((result) => { 
-            if (result instanceof Error) {
-                alert(result.message);
-            } else {
-                console.log(result);
-            }
-        })
+
+        debounce(() => {
+            /* usr o then trás o tipo do erro*/
+            PessoasServices.getAll(1, busca).then((result) => {
+                if (result instanceof Error) {
+                    alert(result.message);
+                } else {
+                    console.log(result);
+                }
+            })
+        });
     }, [busca]);
 
-    return(
+    return (
         <LayoutBaseDePagina
-        titulo="Listagem de pessoas"
-        barraDeFerramentas={<FerramentasDaListagem
-            mostraInputBusca
-            textoBotaoNovo="Nova"
-            textoDeBusca={busca}
-            aoMudarTextoDeBusca={texto => setSearchParams({busca: texto}, {replace: true})}
-        />}
+            titulo="Listagem de pessoas"
+            barraDeFerramentas={<FerramentasDaListagem
+                mostraInputBusca
+                textoBotaoNovo="Nova"
+                textoDeBusca={busca}
+                aoMudarTextoDeBusca={texto => setSearchParams({ busca: texto }, { replace: true })}
+            />}
         >
 
         </LayoutBaseDePagina>
